5217222667602f95bb5f9c1fa6eed0a22b93954b
Harshita <ranjan19harshita@gmail.com>
2024-08-01 15:36:34 +0530

    first commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Pink Screen</h1>
+      <p className='mb-4 text-slate-700'>Finding the right place to stay can make all the difference in your travel experience. At Pink Screen, we've made it easy for women to discover hostels that suit their needs and preferences. From bustling city centers to tranquil retreats, our listings offer a variety of options to fit your style and budget.</p>
+      <p className='mb-4 text-slate-700'>
+      Explore detailed descriptions, user reviews, and photos to make an informed choice. Wherever your travels take you, we have the perfect hostel waiting.  </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}
